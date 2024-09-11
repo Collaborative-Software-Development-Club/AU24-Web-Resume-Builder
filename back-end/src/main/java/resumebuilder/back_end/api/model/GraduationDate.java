61ba773c9d6b077b73beb4b676fbfd8886d67e70
Lorenzo Mion <lorenzodfmion@gmail.com>
2024-09-11 18:31:44 -0400

    added placeholder class for education

---
+++ back-end/src/main/java/resumebuilder/back_end/api/model/GraduationDate.java
@@ -0,0 +1,6 @@
+package resumebuilder.back_end.api.model;
+
+public class GraduationDate {
+    private int month;
+    private int year;
+}
--- back-end/src/main/java/resumebuilder/back_end/api/model/Resume.java
+++ back-end/src/main/java/resumebuilder/back_end/api/model/Resume.java
@@ -1,7 +1,14 @@
 package resumebuilder.back_end.api.model;
 
 public class Resume {
-    public String jsonPlaceholder;
+    /*
+     * Complete this model as you guys work on the endpoints.
+     * I believe the best way for now would be to create a field that has a
+     * type/model of a certain section. For example, education, work experience,
+     * etc.
+     * Also, changing these to interfaces might be good later.
+     */
+    private Education education;
 
     public Resume() {
     }
