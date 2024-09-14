24ae8bed86cd89bb7571022ab136d9e66ceb25cd
Lorenzo Mion <lorenzodfmion@gmail.com>
2024-09-13 20:59:29 -0400

    created the first version of the name and contact methods

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,13 +1,12 @@
-import '@/App.css'
-import ResumeBuilder from '@/pages/ResumeBuilder'
+import '@/App.css';
+import ResumeBuilder from '@/pages/ResumeBuilder';
 
 function App() {
-  return (
-    <div className='flex flex-col justify-center items-center p-10'>
-        <h1 className="text-2xl font-bold">AU24 resume builder project</h1>
-        <ResumeBuilder />
-    </div>
-  )
+    return (
+        <div className="flex flex-col items-center justify-center p-10">
+            <ResumeBuilder />
+        </div>
+    );
 }
 
-export default App
+export default App;
