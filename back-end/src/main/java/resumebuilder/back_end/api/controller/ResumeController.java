ba401ddea3d66df1a9c4f91817572f55909d7dfb
Lorenzo Mion <lorenzodfmion@gmail.com>
2024-09-19 17:31:47 -0400

    refactored controller, resume, and service for the service to use an array of resumes which contain the experiences

--- back-end/src/main/java/resumebuilder/back_end/api/controller/ResumeController.java
+++ back-end/src/main/java/resumebuilder/back_end/api/controller/ResumeController.java
@@ -27,15 +27,19 @@ public class ResumeController {
         return ResponseEntity.ok("the api is working");
     }
 
-    @GetMapping("/experiences")
-    public ResponseEntity<List<Experience>> getExperiences() {
-        return new ResponseEntity<>(resumeService.getExperiences(), HttpStatus.OK);
+    @GetMapping("/{resumeId}/experiences")
+    public ResponseEntity<List<Experience>> getExperiences(@PathVariable("resumeId") int resumeId) {
+        Optional<List<Experience>> optional = resumeService.getExperiences(resumeId);
+        if (optional.isPresent()) {
+            return new ResponseEntity<List<Experience>>(optional.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @GetMapping("/experiences/{id}")
-    public ResponseEntity<Experience> getExperience(@PathVariable("id") int id) {
-        Optional<Experience> experience = resumeService.getExperience(id);
-
+    @GetMapping("/{resumeId}/experiences/{experienceId}")
+    public ResponseEntity<Experience> getExperience(@PathVariable("resumeId") int resumeId,
+            @PathVariable("experienceId") int experienceId) {
+        Optional<Experience> experience = resumeService.getExperience(resumeId, experienceId);
         if (experience.isPresent()) {
             return new ResponseEntity<>(experience.get(), HttpStatus.OK);
         } else {
@@ -43,49 +47,54 @@ public class ResumeController {
         }
     }
 
-    @PostMapping("/experiences")
-    public ResponseEntity<Experience> createExperience(@RequestBody Experience experience) {
-        resumeService.createExperience(experience);
+    @PostMapping("/{resumeId}/experiences")
+    public ResponseEntity<Experience> createExperience(@PathVariable("resumeId") int resumeId,
+            @RequestBody Experience experience) {
+        // TODO this doesn't throw an error if the resume doesn't exist
+        // ! is taking in an id, but the id should be created on the back-end
+        resumeService.createExperience(resumeId, experience);
         return new ResponseEntity<>(experience, HttpStatus.CREATED);
     }
 
-    @PutMapping(path = "/experiences/{id}")
+    @PutMapping("{resumeId}/experiences/{id}")
     public ResponseEntity<Experience> updateExperience(
-            @PathVariable("id") int id, @RequestBody Experience updatedExperience
-    ) {
-        Optional<Experience> existingExperienceOpt = resumeService.getExperience(id);
-
+            @PathVariable("resumeId") int resumeId, @RequestBody Experience updatedExperience) {
+        Optional<Experience> existingExperienceOpt = resumeService.getExperience(resumeId, updatedExperience.getId());
         if (!existingExperienceOpt.isPresent()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
-        Experience updated = resumeService.updateExperience(id, updatedExperience);
+        Optional<Experience> updated = resumeService.updateExperience(resumeId, updatedExperience.getId(),
+                updatedExperience);
+        if (updated.isPresent()) {
+            return new ResponseEntity<>(updated.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 
-        return new ResponseEntity<>(updated, HttpStatus.OK);
     }
 
-    @PatchMapping(path = "/experiences/{id}")
+    @PatchMapping("{resumeId}/experiences/{experienceId}")
     public ResponseEntity<Experience> patchExperience(
-            @PathVariable("id") int id, @RequestBody Map<String, Object> updates
-    ) {
+            @PathVariable("resumeId") int resumeId, @PathVariable("experienceId") int experienceId,
+            @RequestBody Map<String, Object> updates) {
         // Fetch the existing experience
-        Optional<Experience> existingExperienceOpt = resumeService.getExperience(id);
+        Optional<Experience> existingExperienceOpt = resumeService.getExperience(resumeId, experienceId);
 
         if (!existingExperienceOpt.isPresent()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
         // Partially update the experience
-        Experience updatedExperience = resumeService.partialUpdateExperience(id, updates);
+        Experience updatedExperience = resumeService.partialUpdateExperience(resumeId, experienceId, updates);
 
         return new ResponseEntity<>(updatedExperience, HttpStatus.OK);
     }
 
-    @DeleteMapping(path = "/experiences/{id}")
-    public ResponseEntity<Experience> deleteExperience(@PathVariable("id") int id) {
-        Optional<Experience> deletedExperience = resumeService.deleteExperience(id);
-
-        if(deletedExperience.isPresent()) {
+    @DeleteMapping("{resumeId}/experiences/{experienceId}")
+    public ResponseEntity<Experience> deleteExperience(@PathVariable("resumeId") int resumeId,
+            @PathVariable("experienceId") int experienceId) {
+        Optional<Experience> deletedExperience = resumeService.deleteExperience(resumeId, experienceId);
+        if (deletedExperience.isPresent()) {
             return new ResponseEntity<>(deletedExperience.get(), HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
--- back-end/src/main/java/resumebuilder/back_end/api/model/Resume.java
+++ back-end/src/main/java/resumebuilder/back_end/api/model/Resume.java
@@ -1,6 +1,7 @@
 package resumebuilder.back_end.api.model;
 
-import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class Resume {
     /*
@@ -10,19 +11,46 @@ public class Resume {
      * etc.
      * Also, changing these to interfaces might be good later.
      */
-    private Education education;
-    
-    private ArrayList<Experience> experience;
+    private static int idCounter = 0;
+    private int id;
+    private List<Experience> experiences;
 
-    public Resume(ArrayList<Experience> experience) {
-        this.experience = experience;
+    public Resume() {
+        this.id = idCounter;
+        idCounter++;
     }
 
-    public ArrayList<Experience> getExperience() {
-        return experience;
+    public int getId() {
+        return this.id;
     }
 
-    public void setExperience(ArrayList<Experience> experience) {
-        this.experience = experience;
+    public void setExperiences(List<Experience> experiences) {
+        this.experiences = experiences;
+    }
+
+    public void addExperience(Experience newExperience) {
+        experiences.add(newExperience);
+    }
+
+    public Optional<Experience> removeExperience(int id) {
+        for (Experience experience : experiences) {
+            if (experience.getId() == id) {
+                experiences.remove(experience);
+                return Optional.of(experience);
+            }
+        }
+        return Optional.empty();
+    }
+    public List<Experience> getExperiences(){
+        return this.experiences;
+    }
+
+    public Optional<Experience> getExperience(int id) {
+        for (Experience experience : experiences) {
+            if (experience.getId() == id) {
+                return Optional.of(experience);
+            }
+        }
+        return Optional.empty();
     }
 }
