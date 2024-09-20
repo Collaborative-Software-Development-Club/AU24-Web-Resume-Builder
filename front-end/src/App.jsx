93b789e06bde47c71df899b7e00fafd1419765b4
Lorenzo Mion <lorenzodfmion@gmail.com>
2024-09-20 18:18:53 -0400

    example of making a pull request

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -4,6 +4,7 @@ import ResumeBuilder from '@/pages/ResumeBuilder';
 function App() {
     return (
         <div className="flex flex-col items-center justify-center p-10">
+            <p>adding something to the code</p>
             <ResumeBuilder />
         </div>
     );
