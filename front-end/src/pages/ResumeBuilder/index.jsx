80b1beb686aca568e9e8115b08ee79c29871b67d
Elijah Mendez <elijahmendez2022@gmail.com>
2024-09-20 19:37:26 -0400

    Created education section.

--- front-end/src/pages/ResumeBuilder/index.jsx
+++ front-end/src/pages/ResumeBuilder/index.jsx
@@ -1,6 +1,7 @@
 import jsonData from '@/../../data/resume.json';
 import {Name} from './Name';
-import {ContactMethods} from './ContactMethods'
+import {ContactMethods} from './ContactMethods';
+import Education from './Education';
 export default function ResumeBuilder() {
     console.log(jsonData);
     const name = jsonData.name;
@@ -9,6 +10,7 @@ export default function ResumeBuilder() {
         <div className="flex flex-col items-center justify-start">
             <Name name={name} />
             <ContactMethods contactMethods={contactMethods} />
+            <Education></Education>
         </div>
-    )
+    );
 }
