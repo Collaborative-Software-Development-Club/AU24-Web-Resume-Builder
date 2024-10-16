4bcb89a2db6680377b00ff87d7daed3e276a653d
Elijah Mendez <elijahmendez2022@gmail.com>
2024-10-15 23:23:58 -0400

    Small change to experiences component.

--- front-end/src/pages/ResumeBuilder/Experiences.jsx
+++ front-end/src/pages/ResumeBuilder/Experiences.jsx
@@ -3,11 +3,11 @@ import {Experience} from './Experience';
 
 export function Experiences({experiences}) {
     return (
-        <div className="times mt-6 mb-6"> {/* Added mb-6 for margin-bottom */}
+        <div className="times mt-6 mb-6">
             <SectionTitle title="Experiences" />
             <div className="flex flex-col gap-8">
                 {experiences.items
-                    .filter((experience) => experience.visible) // Only show visible experiences
+                    .filter((experience) => experience.visible)
                     .map((experience) => (
                         <Experience key={experience.position} experience={experience} />
                     ))}
