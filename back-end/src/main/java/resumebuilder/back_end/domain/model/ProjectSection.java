bad80c271a5039716b29f4b1c6d10561524faffb
Lorenzo Mion <lorenzodfmion@gmail.com>
2024-10-23 18:03:37 -0400

    added projects section to resume entity and dto, adjusted experience section to remove unnecessary methods

--- back-end/src/main/java/resumebuilder/back_end/domain/model/ProjectSection.java
+++ back-end/src/main/java/resumebuilder/back_end/domain/model/ProjectSection.java
@@ -1,4 +1,16 @@
 package resumebuilder.back_end.domain.model;
 
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProjectSection {
+    private List<Project> items;
 }
