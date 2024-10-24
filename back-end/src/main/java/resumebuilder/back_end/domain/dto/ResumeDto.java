b2518a21daddb8698e6e8a0bd4cfb8962d3bc588
Kenny Beaverson <140558157+kbeaverson@users.noreply.github.com>
2024-10-23 21:20:59 -0400

    Implement order of sections (#32)
    * Implement order of sections
    
    * merged dev and fixed partial update
    
    ---------
    
    Co-authored-by: Lorenzo Mion <lorenzodfmion@gmail.com>

--- back-end/src/main/java/resumebuilder/back_end/domain/dto/ResumeDto.java
+++ back-end/src/main/java/resumebuilder/back_end/domain/dto/ResumeDto.java
@@ -1,18 +1,19 @@
 package resumebuilder.back_end.domain.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import resumebuilder.back_end.domain.model.Resume;
+import resumebuilder.back_end.domain.model.enums.SectionNames;
 
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public class ResumeDto extends Resume {
 
     private String id;
+
 
 }
