a3feae4b4ffcdd34dfb801e1220082a9d466b446
JunhoHwoang <154561586+JunhoHwoang@users.noreply.github.com>
2024-11-06 19:47:32 -0500

    Refactored Drag and drop list to new library and Added back StrictMode

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -4,5 +4,7 @@ import App from '@/App.jsx';
 import '@/index.css';
 
 createRoot(document.getElementById('root')).render(
-        <App /> 
+    <StrictMode>
+        <App />
+    </StrictMode>,
 );
