a2f0aa59e33b62b69d9261c876986429a3268852
Megan L <61627647+Bembb@users.noreply.github.com>
2025-02-12 16:10:06 -0500

    Added bulleted input box (#46)
    * Modified sidebar to have a mobile version (using Shadcn Sheet) and desktop version
    
    * Added "Set Visibility" title back to sidebar
    
    * first version of front-end uploadResumeData
    
    * removed grid stuff on sidebar checkboxes
    
    * added sidebar components to sidebar folder
    
    * Implement resume description field (#40)
    
    * Implement order of sections
    
    * merged dev and fixed partial update
    
    * Implement Resume description field
    
    * first version of front-end uploadResumeData
    
    ---------
    
    Co-authored-by: Lorenzo Mion <lorenzodfmion@gmail.com>
    
    * Implemented Reordering functionality of sections, projects and experience (#39)
    
    * Side bar beta
    
    * Updated Sidebar Component
    
    * Implemented Add/Remove features for Experiences and Projects
    
    * Changed the index to fit both the body and sidebar
    
    * Created a component for drag and drop and connected it to sidebar and editablecomponent
    
    * Changed gpa and honors to match the data
    
    * Reflect the resume object from reordering
    
    * Updated the reordering functionality so it automatically updates the resume object
    
    * Added Visibility funcitonality
    
    * Commiting to merge dev
    
    * Combined new Sidebar design with functionalities
    
    * Completed merging old and new sidebars
    
    * Refactored Drag and drop list to new library and Added back StrictMode
    
    * Changed to new DND library to fix a bug
    
    * Save button for resume - works with Name (#41)
    
    * created AiService. Ollama implementation and mock implementation.
    
    * add back cross origin
    
    * added call to back-end patch method to front-end
    
    * first version of front-end uploadResumeData
    
    * initial version of upload resume data
    
    * merge dev into lorenzo
    
    * added a way to save resume name to db from front-end
    
    * Implement error handling framework (#42)
    
    * Implement order of sections
    
    * merged dev and fixed partial update
    
    * Implement Resume description field
    
    * first version of front-end uploadResumeData
    
    * Implement new error handling framework
    
    ---------
    
    Co-authored-by: Lorenzo Mion <lorenzodfmion@gmail.com>
    
    * Added bulleted input box for experience section
    
    ---------
    
    Co-authored-by: Lorenzo Mion <lorenzodfmion@gmail.com>
    Co-authored-by: Kenny Beaverson <140558157+kbeaverson@users.noreply.github.com>
    Co-authored-by: JunhoHwoang <154561586+JunhoHwoang@users.noreply.github.com>
    Co-authored-by: ldfMion <72419437+ldfMion@users.noreply.github.com>

--- front-end/src/pages/ResumeBuilder/DragAndDropList.jsx
+++ front-end/src/pages/ResumeBuilder/DragAndDropList.jsx
@@ -1,9 +1,9 @@
-import { DragDropContext, Droppable, Draggable } from '@hello-pangea/dnd';
+import {DragDropContext, Droppable, Draggable} from '@hello-pangea/dnd';
 
-export default function DragAndDropList({ array, setArray }) {
+export default function DragAndDropList({array, setArray}) {
     console.log(array);
-    
-    const handleOnDragEnd = ({ source, destination }) => {
+
+    const handleOnDragEnd = ({source, destination}) => {
         if (!destination || source.index === destination.index) return;
 
         const reorderedList = array?.slice();
@@ -11,26 +11,16 @@ export default function DragAndDropList({ array, setArray }) {
         reorderedList.splice(destination.index, 0, removedItem);
         setArray(reorderedList);
     };
-    
 
     return (
         <DragDropContext onDragEnd={handleOnDragEnd}>
             <Droppable droppableId="sections">
                 {(provided) => (
-                    <div
-                        {...provided.droppableProps}
-                        ref={provided.innerRef}
-                        className="flex flex-col gap-2 py-2"
-                    >
+                    <div {...provided.droppableProps} ref={provided.innerRef} className="flex flex-col gap-2 py-2">
                         {array.map((item, index) => (
                             <Draggable key={item.id} draggableId={item.id} index={index}>
                                 {(provided) => (
-                                    <div
-                                        ref={provided.innerRef}
-                                        {...provided.draggableProps}
-                                        {...provided.dragHandleProps}
-                                        className="rounded-lg p-2 shadow"
-                                    >
+                                    <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} className="rounded-lg p-2 shadow">
                                         {item.content}
                                     </div>
                                 )}
