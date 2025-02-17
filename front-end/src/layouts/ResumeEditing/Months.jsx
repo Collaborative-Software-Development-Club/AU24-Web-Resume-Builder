d84572247be8e6ba212a6410cad1980356487924
ldfMion <lorenzodfmion@gmail.com>
2025-02-16 22:10:09 -0500

    made educaiton section edit on click

--- front-end/src/layouts/ResumeEditing/Months.jsx
+++ front-end/src/layouts/ResumeEditing/Months.jsx
@@ -1,29 +1,24 @@
-import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from '@/components/ui/select';
-
-const MONTHS = [
-    {name: 'January', value: 1},
-    {name: 'February', value: 2},
-    {name: 'March', value: 3},
-    {name: 'April', value: 4},
-    {name: 'May', value: 5},
-    {name: 'June', value: 6},
-    {name: 'July', value: 7},
-    {name: 'August', value: 8},
-    {name: 'September', value: 9},
-    {name: 'October', value: 10},
-    {name: 'November', value: 11},
-    {name: 'December', value: 12},
-];
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from '@/components/ui/select';
+import {MONTHS} from '@/constants/months';
 
 export default function Months({type, handleSelectChange, value}) {
     const newHandleSelectChange = (month) => {
         const monthValue = MONTHS.find((m) => m.name === month);
-        handleSelectChange(type.toLowerCase()+"Date", 'month',  Number(monthValue.value));
+        handleSelectChange(type.toLowerCase() + 'Date', 'month', Number(monthValue.value));
     };
 
     return (
         <div className="times">
-            <Select value={MONTHS.find((m) => m.value === value)?.name} onValueChange={newHandleSelectChange}>
+            <Select
+                value={MONTHS.find((m) => m.value === value)?.name}
+                onValueChange={newHandleSelectChange}
+            >
                 <SelectTrigger>
                     <SelectValue placeholder={`${type} Month`} />
                 </SelectTrigger>
