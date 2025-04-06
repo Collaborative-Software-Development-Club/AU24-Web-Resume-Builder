b48ddeda3fa113692949985da77f79bfc4ba636c
ldfMion <lorenzodfmion@gmail.com>
2025-04-05 22:47:32 -0400

    removed the emty div in empty text in BulletPointDisplayView

--- front-end/src/layouts/ResumeEditing/BulletPointDisplayView.jsx
+++ front-end/src/layouts/ResumeEditing/BulletPointDisplayView.jsx
@@ -1,7 +1,5 @@
-export function BulletPointDisplayView({text, placeHolder}) {
-    return text == '' ? (
-        <div></div>
-    ) : (
+export function BulletPointDisplayView({text}) {
+    return text == '' ? null : (
         <ul className="list-disc items-center px-3 py-1 text-sm">
             {text.split('\n').map((item, index) => (
                 <li key={index} className="times w-full">
