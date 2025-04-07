3591cf8e7acdcfb81a83d3e94b63e28b69e703ab
Ford <154796062+4rd2@users.noreply.github.com>
2025-04-06 20:54:02 -0400

    Added Resume Description Box with EditOnClick component in DesktopSidebar

--- front-end/src/layouts/ResumeEditing/Sidebar/DesktopSidebar.jsx
+++ front-end/src/layouts/ResumeEditing/Sidebar/DesktopSidebar.jsx
@@ -8,6 +8,7 @@ import {
     SidebarTrigger,
 } from '@/components/ui/sidebar';
 import {SidebarContent as OurSidebarContent} from './SidebarContent';
+import { EditOnClick } from '../../../components/EditOnClick.jsx';
 
 export const DesktopSidebar = ({
     resume,
@@ -20,6 +21,28 @@ export const DesktopSidebar = ({
         <SidebarProvider>
             <Sidebar>
                 <SidebarContent>
+                    <SidebarGroup>
+                        <SidebarGroupLabel>Resume Description</SidebarGroupLabel>
+                        <SidebarGroupContent>
+                            <hr />
+                            <div className="flex justify-center p-3">
+                                <EditOnClick 
+                                    sectionName="resumeDescription"
+                                    displayView={
+                                    <p className="text-lg text-gray-800 bg-transparent">
+                                        {resume.description || "Click here to add your resume description"}
+
+                                    </p>
+                                    }
+                                    editingView={<textarea 
+                                        className="w-full border-2 rounded bg-transparent"
+                                        defaultValue={resume.description}
+                                        />}
+                                    empty={!resume.description}
+                                />
+                            </div>
+                        </SidebarGroupContent>
+                    </SidebarGroup>
                     <SidebarGroup>
                         <SidebarGroupLabel>Resume Settings</SidebarGroupLabel>
                         <SidebarGroupContent>
